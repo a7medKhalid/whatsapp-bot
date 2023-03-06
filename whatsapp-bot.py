603034270ac98aab3403b8ea06062e18dd61306e
Ahmed <ahmed1alghamdi2000@gmail.com>
2023-03-06 19:09:59 +0300

    find unread conversations only

--- whatsapp-bot.py
+++ whatsapp-bot.py
@@ -1,9 +1,5 @@
 
-# # whatsapp web scraper
-
-import os
-import pickle
-
+# CP: time compare is not working will and i need to unclick the message to make it work again
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
@@ -11,20 +7,96 @@ from selenium.webdriver.common.by import By
 
 # # you can set the chromedriver path on the system path and remove this variable
 CHROMEDRIVER_PATH = 'chromedriver.exe'
-global SCROLL_TO, SCROLL_SIZE
 
 
 # start driver
-# global SCROLL_TO, SCROLL_SIZE
-SCROLL_SIZE = 600
-SCROLL_TO = 600
-conversations = []
 
 options = Options()
 options.add_argument('user-data-dir=./User_Data')  # saving user data so you don't have to scan the QR Code again
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
 driver.get('https://web.whatsapp.com/')
 input('Press enter after scanning QR code or after the page has fully loaded\n')
+
+
+from datetime import datetime
+start_time = datetime.now()
+
+
+messagesDB = []
+
+while True:
+    # scroll sidepane to top
+    sidepane = driver.find_element(By.XPATH, "//div[@class='g0rxnol2 _3fGK2']")
+    driver.execute_script("arguments[0].scrollTop = 0", sidepane)
+
+    # check if there are unread messages
+    unreads = driver.find_elements(By.XPATH , "//span[@class='l7jjieqr cfzgl7ar ei5e7seu h0viaqh7 tpmajp1w c0uhu3dl riy2oczp dsh4tgtl sy6s5v3r gz7w46tb lyutrhe2 qfejxiq4 fewfhwl7 ovhn1urg ap18qm3b ikwl5qvt j90th5db aumms1qt']")
+
+    unreadsCount = len(unreads)
+
+    if unreadsCount == 0 :
+        print('no new messages')
+        # sleep(5)
+        continue
+
+    # get unread conversations
+    conversations = []
+    for unread in unreads:
+
+        unread.click()
+
+        out_messages_xpath = '//div[@class="message-in focusable-list-item _7GVCb _2SnTA _1-FMR"]//div[@class="_1-lf9 _3mSPV _18q-J"]//div[@class="ItfyB _3nbHh"]//div[@class="_27K43"]//div[@class="copyable-text"]'
+        messages = driver.find_elements(By.XPATH, out_messages_xpath + '//div[@class="_21Ahp"]' )
+        times = driver.find_elements(By.XPATH, out_messages_xpath )
+
+        for message in messages:
+
+            time_stamp = times[messages.index(message)].get_attribute('data-pre-plain-text')
+            message = message.text + ',' + str(time_stamp)
+
+            # compare time
+            text = time_stamp
+
+            time  = text.split(']')[0].replace('[', '')
+
+            date = time.split('،')[1].strip()
+            time = time.split('،')[0].strip()
+            # change arabic numbers to english
+            time = time.replace('٠', '0').replace('١', '1').replace('٢', '2').replace('٣', '3').replace('٤', '4').replace('٥', '5').replace('٦', '6').replace('٧', '7').replace('٨', '8').replace('٩', '9').replace('م', 'PM').replace('ص', 'AM')
+            date = date.replace('٠', '0').replace('١', '1').replace('٢', '2').replace('٣', '3').replace('٤', '4').replace('٥', '5').replace('٦', '6').replace('٧', '7').replace('٨', '8').replace('٩', '9')
+
+
+            message_time = time  + ',' + date
+            # conver to datetime
+            message_time = datetime.strptime(message_time, "%I:%M %p,%Y/%m/%d")
+            
+            # compare two times if meeage time is older print yes
+            time_compare =  message_time > start_time
+            print(time_compare,message)
+            
+
+
+
+            # check if message is not read send resopnse
+            if message not in messagesDB and time_compare:
+                
+                msg_box = driver.find_element(By.CSS_SELECTOR, '.fd365im1.to2l77zo.bbv8nyr4.gfz4du6o.ag5g9lrv.bze30y65.kao4egtt')
+                print('sending response')
+                # msg_box.send_keys('hi i am a b-o-t')
+                # msg_box.send_keys(u'\ue007')
+                messagesDB.append(message)
+            
+            # unclick conversation
+            
+
+        # return side pane to top to not miss selinum cordinations
+        sidepane = driver.find_element(By.XPATH, "//div[@class='g0rxnol2 _3fGK2']")
+        driver.execute_script("arguments[0].scrollTop = 0", sidepane)
+        
+
+
+
+
 
 # fetch unread conversations count
 
@@ -58,70 +130,76 @@ input('Press enter after scanning QR code or after the page has fully loaded\n')
 #  todos
 # after counter is 2 the driver does not read messages 
 
-# try to fetch conversation from unread indicator
+# try to fetch conversation from unread indicator after recording all conversatins messages
 
 # prssist messages DB
 
 
-messagesDB = []   
-counter = 0
-while True:
-    # scroll sidepane to top
-    sidepane = driver.find_element(By.XPATH, "//div[@class='g0rxnol2 _3fGK2']")
-    driver.execute_script("arguments[0].scrollTop = 0", sidepane)
+# new scenario 
+# get messages with unread indicator and send response
+# know if message is read or not by checking the time stamp to time stamp of starting the program ifbefore read if after check messages DB if not in DB send response and add to DB
 
-    unreads = driver.find_elements(By.XPATH , "//span[@class='l7jjieqr cfzgl7ar ei5e7seu h0viaqh7 tpmajp1w c0uhu3dl riy2oczp dsh4tgtl sy6s5v3r gz7w46tb lyutrhe2 qfejxiq4 fewfhwl7 ovhn1urg ap18qm3b ikwl5qvt j90th5db aumms1qt']")
 
-    unreadsCount = len(unreads)
+# register time stamp of starting the program with date
+# messagesDB = []   
+# counter = 0
+# while True:
+#     # scroll sidepane to top
+#     sidepane = driver.find_element(By.XPATH, "//div[@class='g0rxnol2 _3fGK2']")
+#     driver.execute_script("arguments[0].scrollTop = 0", sidepane)
 
-    if unreadsCount == 0 and counter > 0:
-        print('no new messages')
-        # sleep(5)
-        continue
+#     unreads = driver.find_elements(By.XPATH , "//span[@class='l7jjieqr cfzgl7ar ei5e7seu h0viaqh7 tpmajp1w c0uhu3dl riy2oczp dsh4tgtl sy6s5v3r gz7w46tb lyutrhe2 qfejxiq4 fewfhwl7 ovhn1urg ap18qm3b ikwl5qvt j90th5db aumms1qt']")
+
+#     unreadsCount = len(unreads)
+
+#     if unreadsCount == 0 and counter > 0:
+#         print('no new messages')
+#         # sleep(5)
+#         continue
 
    
-    # get all conversations
-    conversations = driver.find_elements(By.XPATH, "//span[@class='_7T_0D']//span[@class='ggj6brxn gfz4du6o r7fjleex g0rxnol2 lhj4utae le5p0ye3 l7jjieqr _11JPr']")
+#     # get all conversations
+#     conversations = driver.find_elements(By.XPATH, "//span[@class='_7T_0D']//span[@class='ggj6brxn gfz4du6o r7fjleex g0rxnol2 lhj4utae le5p0ye3 l7jjieqr _11JPr']")
 
-    for conversation in conversations:
-        conversationtext = conversation.text
+#     for conversation in conversations:
+#         conversationtext = conversation.text
 
-        # try:
-        conversation.click()
-        # except:
-        #     conversation = conversation.find_element(By.XPATH, "//span[@title='" + conversation.text + "']")
-        #     conversation.click()
+#         # try:
+#         conversation.click()
+#         # except:
+#         #     conversation = conversation.find_element(By.XPATH, "//span[@title='" + conversation.text + "']")
+#         #     conversation.click()
         
-        out_messages_xpath = '//div[@class="message-in focusable-list-item _7GVCb _2SnTA _1-FMR"]//div[@class="_1-lf9 _3mSPV _18q-J"]//div[@class="ItfyB _3nbHh"]//div[@class="_27K43"]//div[@class="copyable-text"]'
-        messages = driver.find_elements(By.XPATH, out_messages_xpath + '//div[@class="_21Ahp"]' )
-        times = driver.find_elements(By.XPATH, out_messages_xpath )
+#         out_messages_xpath = '//div[@class="message-in focusable-list-item _7GVCb _2SnTA _1-FMR"]//div[@class="_1-lf9 _3mSPV _18q-J"]//div[@class="ItfyB _3nbHh"]//div[@class="_27K43"]//div[@class="copyable-text"]'
+#         messages = driver.find_elements(By.XPATH, out_messages_xpath + '//div[@class="_21Ahp"]' )
+#         times = driver.find_elements(By.XPATH, out_messages_xpath )
 
-        for message in messages:
+#         for message in messages:
 
-            time = times[messages.index(message)].get_attribute('data-pre-plain-text')
-            message = message.text + ',' + str(time)
+#             time = times[messages.index(message)].get_attribute('data-pre-plain-text')
+#             message = message.text + ',' + str(time)
 
-            if message not in messagesDB:
-                # here you can add your response
-                messagesDB.append(message)
-                print(message)
-                print('----------------')
-                # unreadsCount -= 1
-                if counter > 0:
-                    # send response
-                    msg_box = driver.find_element(By.CSS_SELECTOR, '.fd365im1.to2l77zo.bbv8nyr4.gfz4du6o.ag5g9lrv.bze30y65.kao4egtt')
-                    print('sending response')
-                    # msg_box.send_keys('hi i am a b-o-t')
-                    # msg_box.send_keys(u'\ue007')
+#             if message not in messagesDB:
+#                 # here you can add your response
+#                 messagesDB.append(message)
+#                 print(message)
+#                 print('----------------')
+#                 # unreadsCount -= 1
+#                 if counter > 0:
+#                     # send response
+#                     msg_box = driver.find_element(By.CSS_SELECTOR, '.fd365im1.to2l77zo.bbv8nyr4.gfz4du6o.ag5g9lrv.bze30y65.kao4egtt')
+#                     print('sending response')
+#                     # msg_box.send_keys('hi i am a b-o-t')
+#                     # msg_box.send_keys(u'\ue007')
 
                 
 
-            else:
-                break
-        # this code is to save iterations
-        # if unreadsCount == 0 and counter > 1:
-        #     break
-    counter += 1
+#             else:
+#                 break
+#         # this code is to save iterations
+#         # if unreadsCount == 0 and counter > 1:
+#         #     break
+#     counter += 1
 
 
 
