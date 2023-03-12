1a648b078033e9161c123b88222465f4fa147def
Ahmed <ahmed1alghamdi2000@gmail.com>
2023-03-12 05:10:35 +0300

    whatsbo now can read messages

--- whatsapp-bot.py
+++ whatsapp-bot.py
@@ -1,11 +1,11 @@
 
-# CP: time compare is not working will and i need to unclick the message to make it work again
+# CP: i need to unclick the message to make it work again
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 from selenium.webdriver.common.by import By
 
-# # you can set the chromedriver path on the system path and remove this variable
+# you can set the chromedriver path on the system path and remove this variable
 CHROMEDRIVER_PATH = 'chromedriver.exe'
 
 
@@ -27,7 +27,7 @@ print(start_time)
 
 
 messagesDB = []
-
+counter = 0
 while True:
     # scroll sidepane to top
     sidepane = driver.find_element(By.XPATH, "//div[@class='g0rxnol2 _3fGK2']")
@@ -39,8 +39,14 @@ while True:
     unreadsCount = len(unreads)
 
     if unreadsCount == 0 :
-        print('no new messages')
-        # sleep(5)
+        counter += 1
+        
+        if counter == 5:
+            sleep(5)
+            counter = 0
+            print('no new messages')
+
+        
         continue
 
     # get unread conversations
@@ -54,7 +60,7 @@ while True:
         times = driver.find_elements(By.XPATH, out_messages_xpath )
 
         for message in messages:
-
+            message_text = message.text
             time_stamp = times[messages.index(message)].get_attribute('data-pre-plain-text')
             message = message.text + ',' + str(time_stamp)
 
@@ -81,19 +87,46 @@ while True:
             
 
 
-
+            msg_box = driver.find_elements(By.CSS_SELECTOR, '.to2l77zo.gfz4du6o.ag5g9lrv.bze30y65.kao4egtt')
+            msg_box = msg_box[1]
             # check if message is not read send resopnse
             if message not in messagesDB and time_compare:
-                
-                msg_box = driver.find_element(By.CSS_SELECTOR, '.fd365im1.to2l77zo.bbv8nyr4.gfz4du6o.ag5g9lrv.bze30y65.kao4egtt')
-                print('sending response')
-                msg_box.send_keys('hi i am a b-o-t')
+
+                # get user input 
+                if message_text == 'مرحبا':
+                    msg_box.send_keys('مرحبا بك' + '\n')
+                elif message_text == 'حكمة':
+                    # get random quote from api
+                    import requests
+                    import json
+                    import random
+
+                    url = "https://type.fit/api/quotes"
+                    response = requests.get(url)
+                    quotes = json.loads(response.text)
+                    quote = random.choice(quotes)
+                    msg_box.send_keys(quote['text'] + '\n' + quote['author'])
+                else:
+                    msg_box.send_keys('أرسل حكمة لتصلك حكمة')
+
+
                 msg_box.send_keys(u'\ue007')
                 messagesDB.append(message)
-            
-            # unclick conversation
-            
+                    
+                   
+                
 
+            
+        # unclick conversation by clicking esc
+        try:
+            msg_box.send_keys(u'\ue00c')
+        except:
+            msg_box = driver.find_elements(By.CSS_SELECTOR, '.to2l77zo.gfz4du6o.ag5g9lrv.bze30y65.kao4egtt')
+            msg_box = msg_box[1]
+            msg_box.send_keys(u'\ue00c')
+            
+        
+            
         # return side pane to top to not miss selinum cordinations
         sidepane = driver.find_element(By.XPATH, "//div[@class='g0rxnol2 _3fGK2']")
         driver.execute_script("arguments[0].scrollTop = 0", sidepane)
